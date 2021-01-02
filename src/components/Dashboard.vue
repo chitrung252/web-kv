a46f332bdfd79b0a3ac031450ce4188c17fd69ff
son <son-decor>
2021-01-02 16:30:15 +0700

    update module fe

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,89 +1,6 @@
 <template>
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <title>DentaCare - Free Bootstrap 4 Template by Colorlib</title>
-      <meta charset="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no"
-      />
-      <!--     
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700" rel="stylesheet">
-
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/animate.css">
-    
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/magnific-popup.css">
-
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/aos.css">
-
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/ionicons.min.css">
-
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/jquery.timepicker.css">
-
-    
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/flaticon.css">
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/icomoon.css">
-    <link rel="stylesheet" href="<%= BASE_URL %>assets/css/style.css"> -->
-    </head>
     <body>
-      <nav
-        class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
-        id="ftco-navbar"
-      >
-        <div class="container">
-          <a class="navbar-brand" href="index.html">Denta<span>Care</span></a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#ftco-nav"
-            aria-controls="ftco-nav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="oi oi-menu"></span> Menu
-          </button>
-
-          <div class="collapse navbar-collapse" id="ftco-nav">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a href="index.html" class="nav-link">Home</a>
-              </li>
-              <li class="nav-item">
-                <a href="about.html" class="nav-link">About</a>
-              </li>
-              <li class="nav-item">
-                <a href="services.html" class="nav-link">Services</a>
-              </li>
-              <li class="nav-item">
-                <a href="doctors.html" class="nav-link">Doctors</a>
-              </li>
-              <li class="nav-item">
-                <a href="blog.html" class="nav-link">Blog</a>
-              </li>
-              <li class="nav-item">
-                <a href="contact.html" class="nav-link">Contact</a>
-              </li>
-              <li class="nav-item cta">
-                <a
-                  href="contact.html"
-                  class="nav-link"
-                  data-toggle="modal"
-                  data-target="#modalRequest"
-                  ><span>Make an Appointment</span></a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- END nav -->
-
+      <!-- Nav -->
       <section class="home-slider owl-carousel">
         <div
           class="slider-item"
@@ -1199,165 +1116,11 @@
 
       <div id="map"></div>
 
-      <footer class="ftco-footer ftco-bg-dark ftco-section">
-        <div class="container">
-          <div class="row mb-5">
-            <div class="col-md-3">
-              <div class="ftco-footer-widget mb-4">
-                <h2 class="ftco-heading-2">DentaCare.</h2>
-                <p>
-                  Far far away, behind the word mountains, far from the
-                  countries Vokalia and Consonantia, there live the blind texts.
-                </p>
-              </div>
-              <ul
-                class="ftco-footer-social list-unstyled float-md-left float-lft"
-              >
-                <li class="ftco-animate">
-                  <a href="#"><span class="icon-twitter"></span></a>
-                </li>
-                <li class="ftco-animate">
-                  <a href="#"><span class="icon-facebook"></span></a>
-                </li>
-                <li class="ftco-animate">
-                  <a href="#"><span class="icon-instagram"></span></a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-md-2">
-              <div class="ftco-footer-widget mb-4 ml-md-5">
-                <h2 class="ftco-heading-2">Quick Links</h2>
-                <ul class="list-unstyled">
-                  <li><a href="#" class="py-2 d-block">About</a></li>
-                  <li><a href="#" class="py-2 d-block">Features</a></li>
-                  <li><a href="#" class="py-2 d-block">Projects</a></li>
-                  <li><a href="#" class="py-2 d-block">Blog</a></li>
-                  <li><a href="#" class="py-2 d-block">Contact</a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-md-4 pr-md-4">
-              <div class="ftco-footer-widget mb-4">
-                <h2 class="ftco-heading-2">Recent Blog</h2>
-                <div class="block-21 mb-4 d-flex">
-                  <a
-                    class="blog-img mr-4"
-                    style="background-image: url(./assets/images/image_1.jpg)"
-                  ></a>
-                  <div class="text">
-                    <h3 class="heading">
-                      <a href="#"
-                        >Even the all-powerful Pointing has no control about</a
-                      >
-                    </h3>
-                    <div class="meta">
-                      <div>
-                        <a href="#"
-                          ><span class="icon-calendar"></span> Sept 15, 2018</a
-                        >
-                      </div>
-                      <div>
-                        <a href="#"><span class="icon-person"></span> Admin</a>
-                      </div>
-                      <div>
-                        <a href="#"><span class="icon-chat"></span> 19</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="block-21 mb-4 d-flex">
-                  <a
-                    class="blog-img mr-4"
-                    style="background-image: url(./assets/images/image_2.jpg)"
-                  ></a>
-                  <div class="text">
-                    <h3 class="heading">
-                      <a href="#"
-                        >Even the all-powerful Pointing has no control about</a
-                      >
-                    </h3>
-                    <div class="meta">
-                      <div>
-                        <a href="#"
-                          ><span class="icon-calendar"></span> Sept 15, 2018</a
-                        >
-                      </div>
-                      <div>
-                        <a href="#"><span class="icon-person"></span> Admin</a>
-                      </div>
-                      <div>
-                        <a href="#"><span class="icon-chat"></span> 19</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="ftco-footer-widget mb-4">
-                <h2 class="ftco-heading-2">Office</h2>
-                <div class="block-23 mb-3">
-                  <ul>
-                    <li>
-                      <span class="icon icon-map-marker"></span
-                      ><span class="text"
-                        >203 Fake St. Mountain View, San Francisco, California,
-                        USA</span
-                      >
-                    </li>
-                    <li>
-                      <a href="#"
-                        ><span class="icon icon-phone"></span
-                        ><span class="text">+2 392 3929 210</span></a
-                      >
-                    </li>
-                    <li>
-                      <a href="#"
-                        ><span class="icon icon-envelope"></span
-                        ><span class="text">info@yourdomain.com</span></a
-                      >
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 text-center">
-              <p>
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                <!-- Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> -->
-                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              </p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <!-- <Footer/> -->
+      <!-- <Footer/> -->
 
       <!-- loader -->
-      <div id="ftco-loader" class="show fullscreen">
-        <svg class="circular" width="48px" height="48px">
-          <circle
-            class="path-bg"
-            cx="24"
-            cy="24"
-            r="22"
-            fill="none"
-            stroke-width="4"
-            stroke="#eeeeee"
-          />
-          <circle
-            class="path"
-            cx="24"
-            cy="24"
-            r="22"
-            fill="none"
-            stroke-width="4"
-            stroke-miterlimit="10"
-            stroke="#F96D00"
-          />
-        </svg>
-      </div>
+      <Loader/>
 
       <!-- Modal -->
       <div
@@ -1468,15 +1231,15 @@
   <script src="<%= BASE_URL %>assets/js/google-map.js"></script>
   <script src="<%= BASE_URL %>assets/js/main.js"></script> -->
     </body>
-  </html>
 </template>
 
 <script>
+import Loader from '@/components/Loader.vue'
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  name: "Dashboard",
+  components: {
+    Loader
+  }
 };
 </script>
 
